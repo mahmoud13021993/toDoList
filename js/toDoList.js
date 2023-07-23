0e1a0e60c354585556a503c36f1893d5f17935f6
mahmoud13021993 <mahmoud.khairy1321993@gmail.com>
2023-07-23 23:46:40 +0300

    updated

--- js/toDoList.js
+++ js/toDoList.js
@@ -23,7 +23,7 @@ addBtn.onclick=function() {
   
 }
 function taskValidation() {
-    let rgex = /^[a-zA-Z0-9]{1,50}$/
+    let rgex = /^[a-z A-Z 0-9]{1,50}$/
     let taskValidat = addTask.value;
     if(rgex.test(taskValidat)) {
         console.log("match")
